c41a497c9d20280949bd9468a0eeee1fa67caed3
AbeerRoy <abir.roy.03.07@gmail.com>
2023-02-03 02:55:46 +0600

    Secret Key encapsulated

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--jpr6$2!x9x!qc7e5fxh=wm=@5varuqbxr-y6(ei(vz@2)+=z^'
+SECRET_KEY = os.environ.get('Secret_Key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
